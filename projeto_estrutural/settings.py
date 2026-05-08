40d2a5d273f31bc088af051aec5afd2f78348d01
Helder Silva <162582989+Hmarttinelle@users.noreply.github.com>
2026-05-08 21:14:23 +0100

    Remove secret key from settings.py
    Removed the production secret key for security reasons.

--- projeto_estrutural/settings.py
+++ projeto_estrutural/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env')) # Adicione esta linha
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-az(0*9m(dq_6l!p04%a+ydn*teowdwr9xt_v&@7u$#@c+(p@if'
+SECRET_KEY=''
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
